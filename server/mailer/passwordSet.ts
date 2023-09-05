60b0698375abd854a45b8d5160e9300b691ff722
Juanjo <juanjovazpar@gmail.com>
2023-09-05 13:33:17 +0200

    Add reset password process

---
+++ server/mailer/passwordSet.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import transporter from "./transporter";
+
+dotenv.config();
+
+export const sendPasswordSet = async (recipient: string): Promise<void> => {
+  const link = `https://example.com/sigin`;
+  const mailOptions = {
+    from: process.env.MAILER_FROM,
+    to: recipient,
+    subject: "Password set successfully",
+    html: `Click the following link to signin in your account: <a href="${link}">SIGN IN</a>`,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
