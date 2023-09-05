0c97edd298d7b2ee22ebd054b2869f10fa16041b
Juanjo <juanjovazpar@gmail.com>
2023-09-05 13:56:04 +0200

    Update categoeies descriptions

--- server/db/createFeatureCategories.ts
+++ server/db/createFeatureCategories.ts
@@ -3,23 +3,41 @@ import { FeatureCategory } from "../models/featureCategory";
 const featureCategoriesData: any[] = [
   {
     name: "Must-Be Quality",
-    description: "Essential features that must be present.",
+    description: `Essential features that must be present:
+      Customers expect these features to be present.
+      Their absence would lead to dissatisfaction.
+      When present, they don't necessarily create excitement, but their absence is a significant source of dissatisfaction.
+      Responses: Expected, not noticed when present, causes dissatisfaction when absent.`,
   },
   {
     name: "One-Dimensional Quality",
-    description: "Features where quality is linearly related to satisfaction.",
+    description: `Features where quality is linearly related to satisfaction.
+      These are features that directly correlate with satisfaction.
+      The better these features are, the more satisfied the customers are.
+      Their absence doesn't necessarily lead to dissatisfaction, but their presence increases satisfaction.
+      Responses: Linear relationship between feature quality and satisfaction.`,
   },
   {
     name: "Attractive Quality",
-    description: "Features that can delight users when present.",
+    description: `Features that delight and surprise customers.
+      Their presence can lead to increased satisfaction and loyalty.
+      Their absence doesn't necessarily cause dissatisfaction; customers may not expect them.
+      Responses: Delight, excitement when present, no dissatisfaction when absent.`,
   },
   {
     name: "Indifferent Quality",
-    description: "Features that don't significantly impact satisfaction.",
+    description: `Features that don't significantly impact satisfaction.
+      These are features that customers neither care about nor are dissatisfied with when they are absent.
+      They have little impact on overall satisfaction.
+      Responses: Neutral, no strong feelings either way.`,
   },
   {
     name: "Reverse Quality",
-    description: "Features that can lead to dissatisfaction when present.",
+    description: `Features that can lead to dissatisfaction when present.
+      Features that, when present, lead to dissatisfaction.
+      Customers prefer these features to be absent.
+      Their presence can be surprising and negatively impact satisfaction.
+      Responses: Dissatisfaction when present, satisfaction when absent.`,
   },
 ];
 
