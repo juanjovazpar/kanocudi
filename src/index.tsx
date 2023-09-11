ba1a45efbddd2e8b218c7560a50736ea87f3f797
Juan Vazquez <juan.vazquez@esgbook.com>
2023-09-11 14:45:24 +0200

    Add first version of Stripe payment

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './custom.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./custom.scss";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <script src="https://js.stripe.com/v3/"></script>
     <App />
   </React.StrictMode>
 );
